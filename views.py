ff2303d706150711ce20d29755554bc67883652e
fraferra <fraferra@cisco.com>
2013-11-03 10:39:21 -0800

    first

---
+++ views.py
@@ -0,0 +1,13 @@
+import os
+import jinja2
+from flask import render_template, request, session, redirect, url_for, escape, Flask
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run()
+
+@app.route('/', methods=['GET'])
+def views():
+    return render_template('index.html')
+
+
